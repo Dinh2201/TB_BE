62c6f82e19cdb5f124dbb414c96172ddc451917f
dinhphamcanh <dinhpc@solashi.com>
2023-12-13 11:10:38 +0700

    first commit

---
+++ routers/event.router.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  create,
+  myEvent,
+  listApproveEvent,
+  approveEvent,
+  listEvent,
+  getEventById,
+  getEventByUserId,
+} = require("../controllers/event.controller");
+
+const asyncMiddelware = require("../middlewares/asyncHandle");
+
+router.route("/my-event/:id").get(asyncMiddelware(myEvent));
+router.route("/approve-event/:id").put(asyncMiddelware(approveEvent));
+router.route("/approve-event").get(asyncMiddelware(listApproveEvent));
+router.route("/search").get(asyncMiddelware(listEvent));
+router.route("/user/:id").get(asyncMiddelware(getEventByUserId));
+router.route("/:id").get(asyncMiddelware(getEventById));
+router.route("/").post(asyncMiddelware(create));
+
+module.exports = router;
